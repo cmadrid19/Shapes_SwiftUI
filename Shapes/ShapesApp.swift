acbbd9273549c74437d84b26d8184d4f43a3dc89
Max <maxim.macari.dam@gmail.com>
2020-11-15 17:28:04 +0100

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Maxim Macari on 15/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
